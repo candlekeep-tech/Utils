917944225b2cbf614d9ccd38081b82e83a417799
Tomas Trojanek <trojanek.tomas@gmail.com>
2020-05-14 12:51:28 +0200

    Create project 'Candlekeep.Utils.Testability' and class 'BuilderBase'

---
+++ src/Candlekeep.Utils/Candlekeep.Utils.Testability/BuilderBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Candlekeep.Utils.Testability
+{
+    /// <summary>
+    /// Base class for builders of test object instances.
+    /// </summary>
+    public abstract class BuilderBase
+    {
+        /// <summary>
+        /// Gets the random generator this builder uses when creating test data.
+        /// </summary>
+        public Random Random { get; }
+
+        protected BuilderBase()
+        {
+            Random = new Random();
+        }
+    }
+}
